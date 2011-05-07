be393ae13e3da99b73de2c73f797333b2594376e
David McNeil <dmcneil@revelytix.com>
2011-05-07 18:48:52 -0500

    switch back to 'normal' matchure
    --HG--
    extra : rebase_source : bead6c20c5a5879f5db20bfa3d16c7005928e84c

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Enhanced clojure records."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [david-mcneil/matchure "0.9.2"]]
+                 [matchure "0.10.1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
